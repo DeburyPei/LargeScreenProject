0a5d644bc21d27b955ec7882a66c3ff740e94126
Debury Pei <svipbighead67@gmail.com>
2023-09-01 15:18:09 +0800

    girds

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,10 +3,18 @@ import './home.scss';
 import headerBg from '../images/header.png'
 export const Home = () => {
   return (
-    <div>
+
       <div className="home">
-        <header style={{backgroundImage:`url(${headerBg})`}}></header>
+        <header style={{backgroundImage:`url(${headerBg})`}}/>
+          <main>
+              <section className="section1"></section>
+              <section className="section2"></section>
+              <section className="section3"></section>
+              <section className="section4"></section>
+              <section className="section5"></section>
+              <section className="section6"></section>
+          </main>
       </div>
-    </div>
+
   );
 };
